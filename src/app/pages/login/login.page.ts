dc36e52c5735980229b8831a11d8cc4db9704c89
Carlyle Komalram <glaw14@gmail.com>
2022-07-19 01:32:02 -0500

    trabajando estilo login a manopla

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -10,7 +10,7 @@ import { Usuario } from 'src/app/interfaces/interfaces';
 })
 export class LoginPage implements OnInit {
 
-  @ViewChild('slidePrincipal') slidePrincipal: IonSlides;
+  // @ViewChild('slidePrincipal') slidePrincipal: IonSlides;
 
   loginUser = {
     email: 'ckomalram@gmail.com',
@@ -34,77 +34,77 @@ export class LoginPage implements OnInit {
   ngOnInit() {
   }
 
-  ionViewDidEnter() {
-    this.slidePrincipal.lockSwipes(true);
-  }
+  // ionViewDidEnter() {
+  //   this.slidePrincipal.lockSwipes(true);
+  // }
 
-  mostrarLogin(){
-    this.slidePrincipal.lockSwipes(false);
-    this.slidePrincipal.slideTo(0);
-    this.slidePrincipal.lockSwipes(true);
+  // mostrarLogin(){
+  //   this.slidePrincipal.lockSwipes(false);
+  //   this.slidePrincipal.slideTo(0);
+  //   this.slidePrincipal.lockSwipes(true);
 
-  }
+  // }
 
-  mostrarRegistro(){
-    this.slidePrincipal.lockSwipes(false);
-    this.slidePrincipal.slideTo(1);
-    this.slidePrincipal.lockSwipes(true);
+  // mostrarRegistro(){
+  //   this.slidePrincipal.lockSwipes(false);
+  //   this.slidePrincipal.slideTo(1);
+  //   this.slidePrincipal.lockSwipes(true);
 
-  }
+  // }
 
-  chooseAvatar(event: string){
-    console.log(event);
-    this.registerUser.avatar=event;
-  }
+  // chooseAvatar(event: string){
+  //   console.log(event);
+  //   this.registerUser.avatar=event;
+  // }
 
-  async login(fLogin: NgForm){
-    console.log(fLogin.valid);
-    console.log(this.loginUser);
-    if (fLogin.invalid) {
-      return;
-    }
+  // async login(fLogin: NgForm){
+  //   console.log(fLogin.valid);
+  //   console.log(this.loginUser);
+  //   if (fLogin.invalid) {
+  //     return;
+  //   }
 
-    // const valido = await this.usuarioService.login(this.loginUser.email,this.loginUser.password);
-    const valido = this.loginUser.profile;
-    if (valido === 'admin') {
-      //navegar a tabs
-      this.navCtrl.navigateRoot('/docente', {
-        animated: true
-      });
-    } else {
-      //mostrar mensaje de credenciales no validad.
-      // this.uiservice.alertaInformativa('Credenciales no validas.');
-      // console.log('Credenciales no validas.');
-      //Esta navgación solo es por demo. aun no se implementa middlewares.
-      this.navCtrl.navigateRoot('/docente', {
-        animated: true
-      });
-    }
+  //   // const valido = await this.usuarioService.login(this.loginUser.email,this.loginUser.password);
+  //   const valido = this.loginUser.profile;
+  //   if (valido === 'admin') {
+  //     //navegar a tabs
+  //     this.navCtrl.navigateRoot('/docente', {
+  //       animated: true
+  //     });
+  //   } else {
+  //     //mostrar mensaje de credenciales no validad.
+  //     // this.uiservice.alertaInformativa('Credenciales no validas.');
+  //     // console.log('Credenciales no validas.');
+  //     //Esta navgación solo es por demo. aun no se implementa middlewares.
+  //     this.navCtrl.navigateRoot('/docente', {
+  //       animated: true
+  //     });
+  //   }
 
-  }
-  async register(fRegister: NgForm){
-  console.log(fRegister);
+  // }
+  // async register(fRegister: NgForm){
+  // console.log(fRegister);
 
-  if (fRegister.invalid) {
-    return;
-  }
+  // if (fRegister.invalid) {
+  //   return;
+  // }
 
-  // const valido = await  this.usuarioService.register(this.registerUser);
-  const valido = this.registerUser.profile;
-  if (valido === 'admin') {
-    //navegar a tabs
-    this.navCtrl.navigateRoot('/docente', {
-      animated: true
-    });
-  } else {
-    //mostrar mensaje de credenciales no validad.
-    // this.uiservice.alertaInformativa('Correo Electronico ya existe');
-    // console.log('Correo Electronico ya existe');
-          //Esta navgación solo es por demo. aun no se implementa middlewares.
-          this.navCtrl.navigateRoot('/docente', {
-            animated: true
-          });
-  }
-  }
+  // // const valido = await  this.usuarioService.register(this.registerUser);
+  // const valido = this.registerUser.profile;
+  // if (valido === 'admin') {
+  //   //navegar a tabs
+  //   this.navCtrl.navigateRoot('/docente', {
+  //     animated: true
+  //   });
+  // } else {
+  //   //mostrar mensaje de credenciales no validad.
+  //   // this.uiservice.alertaInformativa('Correo Electronico ya existe');
+  //   // console.log('Correo Electronico ya existe');
+  //         //Esta navgación solo es por demo. aun no se implementa middlewares.
+  //         this.navCtrl.navigateRoot('/docente', {
+  //           animated: true
+  //         });
+  // }
+  // }
 
 }
