e09f296043fc295f54a38ed81c8f2bcfd3211371
Carlyle Komalram <glaw14@gmail.com>
2022-07-19 20:36:07 -0500

    Login page - animation y css

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { IonSlides, NavController } from '@ionic/angular';
+import { AnimationController,Animation, IonSlides, NavController } from '@ionic/angular';
 import { Usuario } from 'src/app/interfaces/interfaces';
 
 @Component({
@@ -27,11 +27,65 @@ export class LoginPage implements OnInit {
   };
 
   imgfondo ='/assets/login-bg.jpg';
+  registerbtn=true;
 
-
-  constructor(private navCtrl: NavController) { }
+  constructor(private navCtrl: NavController,
+    private animationCtrl: AnimationController
+    ) {  }
 
   ngOnInit() {
+  }
+
+  async tootgleAction(action: boolean){
+
+
+    if (!action) {
+      const elemento = document.querySelector('.boxregister');
+      const animation: Animation = this.animationCtrl.create()
+      .addElement(elemento)
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.2)', opacity: '0.5' },
+        { offset: 1, transform: 'scale(1.5)', opacity: '0.2' }
+      ]);
+      const animation2: Animation = this.animationCtrl.create()
+      .addElement(document.querySelector('#loginbtn'))
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.2)', opacity: '0.5' },
+        { offset: 1, transform: 'scale(1.5)', opacity: '0.2' }
+      ]);
+
+      const parent = this.animationCtrl.create()
+      .duration(800)
+      .addAnimation([animation, animation2]);
+      await  parent.play();
+
+    }else{
+      const elemento = document.querySelector('.boxlogin');
+      const animation: Animation = this.animationCtrl.create()
+      .addElement(elemento)
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.2)', opacity: '0.5' },
+        { offset: 1, transform: 'scale(1.5)', opacity: '0.2' }
+      ]);
+
+      const animation2: Animation = this.animationCtrl.create()
+      .addElement(document.querySelector('#registerbtn'))
+      .keyframes([
+        { offset: 0, transform: 'scale(1)', opacity: '1' },
+        { offset: 0.5, transform: 'scale(1.2)', opacity: '0.5' },
+        { offset: 1, transform: 'scale(1.5)', opacity: '0.2' }
+      ]);
+
+      const parent = this.animationCtrl.create()
+      .duration(800)
+      .addAnimation([animation, animation2]);
+      await  parent.play();
+    }
+
+    this.registerbtn = action;
   }
 
   // ionViewDidEnter() {
@@ -57,31 +111,35 @@ export class LoginPage implements OnInit {
   //   this.registerUser.avatar=event;
   // }
 
-  // async login(fLogin: NgForm){
-  //   console.log(fLogin.valid);
-  //   console.log(this.loginUser);
-  //   if (fLogin.invalid) {
-  //     return;
-  //   }
+  async login(){
 
-  //   // const valido = await this.usuarioService.login(this.loginUser.email,this.loginUser.password);
-  //   const valido = this.loginUser.profile;
-  //   if (valido === 'admin') {
-  //     //navegar a tabs
-  //     this.navCtrl.navigateRoot('/docente', {
-  //       animated: true
-  //     });
-  //   } else {
-  //     //mostrar mensaje de credenciales no validad.
-  //     // this.uiservice.alertaInformativa('Credenciales no validas.');
-  //     // console.log('Credenciales no validas.');
-  //     //Esta navgación solo es por demo. aun no se implementa middlewares.
-  //     this.navCtrl.navigateRoot('/docente', {
-  //       animated: true
-  //     });
-  //   }
+    this.navCtrl.navigateRoot('/docente', {
+      animated: true
+    });
+    // console.log(fLogin.valid);
+    // console.log(this.loginUser);
+    // if (fLogin.invalid) {
+    //   return;
+    // }
 
-  // }
+    // // const valido = await this.usuarioService.login(this.loginUser.email,this.loginUser.password);
+    // const valido = this.loginUser.profile;
+    // if (valido === 'admin') {
+    //   //navegar a tabs
+    //   this.navCtrl.navigateRoot('/docente', {
+    //     animated: true
+    //   });
+    // } else {
+    //   //mostrar mensaje de credenciales no validad.
+    //   // this.uiservice.alertaInformativa('Credenciales no validas.');
+    //   // console.log('Credenciales no validas.');
+    //   //Esta navgación solo es por demo. aun no se implementa middlewares.
+    //   this.navCtrl.navigateRoot('/docente', {
+    //     animated: true
+    //   });
+    // }
+
+  }
   // async register(fRegister: NgForm){
   // console.log(fRegister);
 
